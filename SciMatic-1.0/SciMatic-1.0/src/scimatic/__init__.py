7208e33651509e063a02c5fa1a915ae2c5979526
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-01 16:35:01 +0800

    Add files via upload

---
+++ SciMatic-1.0/SciMatic-1.0/src/scimatic/__init__.py
@@ -0,0 +1,18 @@
+from . import algebra
+from . import geometry
+from . import statistics
+from . import convenient_utils
+from . import errors
+from . import niche
+from .scientific import mechanics
+
+'''
+SciMatic
+A lightweight Python library for
+mathematics, science calculations,
+anf experimental functions.
+'''
+
+__version__ = '1.0.0'
+__author__ = 'Th3Curs3dOn3'
+__license__ = 'MIT'
