f185af5c249b6897a9ed071424c072e4308a5aa2
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-11 07:40:35 +0800

    Update version and author information in __init__.py

--- scimatic/__init__.py
+++ scimatic/__init__.py
@@ -1,11 +1,4 @@
-from .mathematic import algebra
-from .mathematic import geometry
-from .mathematic import statistics
 from . import convenient_utils
-from .mathematic import errors
-from .mathematic import niche
-from .scientific import mechanics
-from .c_engineering import signals, circuits, logic_gates, seg_7display
 
 '''
 SciMatic
@@ -14,6 +7,6 @@ mathematics, science calculations,
 anf experimental functions.
 '''
 
-__version__ = '1.31.2'
+__version__ = '1.32.0'
 __author__ = 'Th3Curs3dOn3'
 __license__ = 'MIT'
