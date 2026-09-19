a99ac3f346555d65c30a3c1f618bff547a95a3e1
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-19 12:31:09 +0800

    Update flicker.rs

--- src/flicker.rs
+++ src/flicker.rs
@@ -1,39 +1,23 @@
 use pyo3::prelude::*;
-use pyo3::types::{PyBool, PyInt, PyList};
+use pyo3::types::PyList;
 
 #[pyfunction]
-#[pyo3(signature = (b, print_result=false))]
-pub fn convert_binary(
+#[pyo3(signature = (vectorbools, binary=false))]
+pub fn flick(
     py: Python<'_>,
-    b: Vec<PyObject>,
-    print_result: bool,
+    vectorbools: Vec<PyObject>,
+    binary: bool,
 ) -> PyResult<Py<PyList>> {
     let output = PyList::empty(py);
 
-    for binary in b {
-        if let Ok(value) = binary.extract::<bool>(py) {
-            output.append(if value { 1 } else { 0 })?;
-        } else if let Ok(value) = binary.extract::<u8>(py) {
-            if value == 1 {
-                output.append(true)?;
-            } else if value == 0 {
-                output.append(false)?;
-            } else {
-                return Err(pyo3::exceptions::PyValueError::new_err(
-                    "Binary values must be 0 or 1",
-                ));
-            }
+    for b in vectorbools {
+        if binary {
+        				let v: u8 = b.extract(py)?;
+            output.append(if b == 0 { 1 } else { 0 })?;
         } else {
-            return Err(pyo3::exceptions::PyTypeError::new_err(
-                "Expected bool or integer",
-            ));
+        				let v: bool = b.extract(py)?;
+            output.append(!)?;
         }
     }
-
-    if print_result {
-        println!("{:?}", output);
-    }
-
     Ok(output.unbind())
-}
 }
