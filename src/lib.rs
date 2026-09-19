638efe5d07ae6ed1e159b80f8a57df0efa3f9d22
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-19 12:31:41 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,16 @@
-use pyo3::prelude::*;
-use pyo3::types::PyList;
+mod flicker;
+mod conversion;
 
-#[pyfunction]
-#[pyo3(signature = (vectorbools, binary=false))]
-fn flick(
-    py: Python<'_>,
-    vectorbools: Vec<bool>,
-    binary: bool,
-) -> PyResult<Py<PyList>> {
-    let output = PyList::empty(py);
-
-    for b in vectorbools {
-        if binary {
-            output.append(if b { 0 } else { 1 })?;
-        } else {
-            output.append(!b)?;
-        }
-    }
-
-    Ok(output.unbind())
-}
+use pyo3::prelude::*;
 
 #[pymodule]
 fn flicker_module(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(flick, m)?)?;
+    m.add_function(wrap_pyfunction!(flicker::flick, m)?)?;
     Ok(())
+}
+
+#[pymodule]
+fn rs_converters(m: &Bound<'_, PyModule>) -> PyResult<()> {
+				m.add_function(wrap_pyfunction!(b2b::convert_binary, m)?)?;
+				Ok(())
 }
