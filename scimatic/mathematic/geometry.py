dc4f3068d18fd7448b06850a445f258f33dadfa7
TheCursedOne <bingchiliingsacker@gmail.com>
2026-09-24 20:07:07 +0800

    Update geometry.py

--- scimatic/mathematic/geometry.py
+++ scimatic/mathematic/geometry.py
@@ -1,5 +1,207 @@
 import math
 from .errors import CaseError, TrigonometryError
+from ..utils.convenient_utils import pi
+from decimal import Decimal
+
+# Circle/spherical logic
+def area_circle(
+	r: Decimal,
+	print_result: bool = False
+) -> Decimal:
+	
+	output = pi() * r**2
+	
+	if print_result:
+		print(output)
+	return output
+
+def circumference_circle(
+	r: Decimal,
+	print_result: bool = False
+) -> Decimal:
+	
+	output = 2 * pi() * r
+	
+	if print_result:
+		print(output)
+	return output
+	
+def volume_circular(
+	shape: str,  # Supports a cylinder, cone, sphere, and hemisphere
+	*args: Decimal,
+	print_result: bool = False
+) -> Decimal:
+	
+	shapes = ['cylinder', 'cone', 'sphere', 'hemisphere']
+    
+	shape = shape.lower()
+	
+	if shape not in shapes:
+		raise ValueError(f'Shapes must be {\',\'.join(shapes)}.')
+	
+	else:
+		if shape == shapes[0]:
+			if len(args) != 2:
+				raise ValueError(f'Cylinder needs 2 arguments, {len(args)} were given.')
+			
+			output = pi() * args[0]**2 * args[1]
+			
+		elif shape == shapes[1]:
+			if len(args) != 2:
+				raise ValueError(f'Cone needs 2 arguments, {len(args)} were given.')
+			
+			output = Decimal('1') / Decimal('3') * pi() * args[0]**2 * args[1]
+
+		elif shape == shapes[2]:
+			if len(args) != 1:
+				raise ValueError(f'Sphere needs 1 argument, {len(args)} were given.')
+			
+			output = Decimal('4') / Decimal('3') * pi() * args[0]**3
+		
+		else:
+			if len(args) != 1:
+				raise ValueError(f'Hemisphere needs 1 argument, {len(args)} were given.')
+			
+			output = Decimal('2') / Decimal('3') * pi() * args[0]**3
+	
+	if print_result:
+		print(output)
+	return output				
+
+# Triangle/Triangular logic
+def perimeter_triangle(
+	a: Decimal,
+	b: Decimal,
+	c: Decimal,
+	print_result: bool = False
+) -> Decimal:
+	
+	output = a + b + c
+	
+	if print_result:
+		print(output)
+	return output
+
+def area_triangle(
+	b: Decimal,
+	h: Decimal,
+	print_result: bool = False
+) -> Decimal:
+	
+	output = Decimal('0.5') * b * h
+	
+	if print_result:
+		print(output)
+	return output
+	
+# Parallelogram logic
+def perimeter_parallelogram(a: Decimal, b: Decimal, print_result: bool = False) -> Decimal:
+	output = Decimal('2') * (a + b)
+	
+	if print_result:
+		print(output)
+	return output
+
+def area_parallelogram(b: Decimal, h: Decimal, print_result: bool = False) -> Decimal:
+	output = b * h
+	
+	if print_result:
+		print(output)
+	return output
+
+# Trapezoin logic
+
+def perimeter_trapezoid(a: Decimal, b: Decimal, c: Decimal, d: Decimal, print_result: bool = False) -> Decimal:
+	output = a + b + c + d
+	
+	if print_result:
+		print(output)
+	return output
+
+def area_trapezoid(a: Decimal, b: Decimal, h: Decimal, print_result: bool = False) -> Decimal:
+	output = Decimal('1') / Decimal('2') * (a + b) * h
+	
+	if print_result:
+		print(output)
+	return output
+
+# Square/quadrilateral logic
+
+def perimeter_square(s: Decimal, print_result: bool = False) -> Decimal:
+	output = Decimal('4') * s
+	
+	if print_result:
+		print(output)
+	return output
+
+def area_square(s: Decimal, print_result: bool = False) -> Decimal:
+	output = s**Decimal('2')
+	
+	if print_result:
+		print(output)
+	return output
+
+# Rectangle logic
+
+def perimeter_rect(l: Decimal, w: Decimal, print_result: bool = False) -> Decimal:
+	output = Decimal('2') * (l + w)
+	
+	if print_result:
+		print(output)
+	return output
+
+def area_rect(l: Decimal, w: Decimal, print_result: bool = False) -> Decimal:
+	output = l * w
+	
+	if print_result:
+		print(output)
+	return output
+
+# Polygon logic
+
+def perimeter_polygon(n: Decimal, s: Decimal, print_result: bool = False) -> Decimal:
+	output = n * s
+	
+	if print_result:
+		print(output)
+	return output
+
+def area_polygon(P: Decimal, a: Decimal, print_result: bool = False) -> Decimal:
+	output = Decimal('1') / Decimal('2') * P * a
+	
+	if print_result:
+		print(output)
+	return output
+
+# Volume logic
+def volume_cube(a: Decimal, print_result: bool = False) -> Decimal:
+	output = a**Decimal('3')
+	
+	if print_result:
+		print(output)
+	return output
+
+def volume_cuboid(l: Decimal, w: Decimal, h: Decimal, print_result: bool = False) -> Decimal:
+	output = l * w * h
+	
+	if print_result:
+		print(output)
+	return output
+
+def volume_prism(A: Decimal, h: Decimal, print_result: bool = False) -> Decimal:
+	output = A * h
+	
+	if print_result:
+		print(output)
+	return output
+
+def volume_pyramid(A: Decimal, h: Decimal, print_result: bool = False) -> Decimal:
+	output = Decimal('1') / Decimal('3') * 	  A * h
+	
+	if print_result:
+		print(output)
+	return output
+
 
 
 def sin_law(
